902029fc11653fae8c14b54d6258b25bff8ce923
Vishnu <vishnukuar5428@gmail.com>
2024-03-12 21:31:07 +0530

    interview

---
+++ 4th_Pillar/InterviewProblem/demo3.js
@@ -0,0 +1,14 @@
+console.log("Hii");
+
+for (let i = 0; i < 3; i++) {
+    setTimeout(function exe() {
+        console.log("timer done");
+    }, 0);
+
+
+}
+
+for (let i = 0; i < 1000000000; i++) {
+    // some tasks
+}
+console.log("done");
